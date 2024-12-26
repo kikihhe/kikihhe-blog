1219c39591310d5a8d7d19c3fdf8eb9221b13abb
kikihhe <heweifei123@gmail.com>
2024-12-27 00:03:01 +0800

    feat: init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ const htmlModules = require('./config/htmlModules.js');
 
 
 module.exports = {
-
   theme: 'vdoing', // 使用依赖包主题
   // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
 
@@ -57,7 +56,7 @@ module.exports = {
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'https://github.com/2382546457', // 导航栏右侧生成Github链接
+    repo: 'https://github.com/kikihhe', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
